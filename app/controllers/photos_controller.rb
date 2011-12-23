6640708bccb4ad829ffc987a3dc2cb6b576af064
Nakamura shinichirou <naka5313@gmail.com>
2011-12-23 11:28:18 +0900

    my photo list use thumb image.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,7 +4,7 @@ class PhotosController < ApplicationController
   # GET /photos
   # GET /photos.json
   def index
-    @photos = Photo.where('user_id=?',session[:user_id])
+    @photos = Photo.where('user_id=?',session[:user_id]).latest.paginate(:page=>params[:page], :per_page=>10)
 
     respond_to do |format|
       format.html # index.html.erb
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ActiveRecord::Base
   belongs_to :album
-  scope :latest  ,order('update_at desc')
+  scope :latest  ,order('updated_at desc')
   validates :title  ,:presence=>true
   has_attached_file :photo,:url=>":rails_root/public:url",:url=>"/system/img/:attaches/:id/:style/:filename" , :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
