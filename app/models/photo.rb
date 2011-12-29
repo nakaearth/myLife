9c288b7818d94a6fc8d1e9ee5938b6f6a8ea936c
Nakamura shinichirou <naka5313@gmail.com>
2011-12-29 23:30:41 +0900

    add js files

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -9,5 +9,5 @@ class Photo < ActiveRecord::Base
   validates :title  ,:presence=>true
   validates_attachment_size :photo ,:in =>1..5.megabyte,:message=>'ファイルサイズが大きすぎます' 
 
-  has_attached_file :photo,:url=>":rails_root/public:url",:url=>"/system/img/:attaches/:id/:style/:filename" , :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :photo,:url=>":rails_root/public:url",:url=>"/system/img/attaches/:id/:style/:filename" , :styles => { :medium => "350x350>", :thumb => "100x100>" }
 end
