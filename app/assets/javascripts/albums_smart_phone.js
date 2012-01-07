2f2a40bc32dfb848ab91042eba6085b0a04f6b3d
Nakamura shinichirou <naka5313@gmail.com>
2012-01-07 15:34:12 +0900

    javascript file and css file updated

--- app/assets/javascripts/albums_smart_phone.js
+++ app/assets/javascripts/albums_smart_phone.js
@@ -3,5 +3,5 @@
 // be included in the compiled file accessible from http://example.com/assets/application.js
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
-//= require smart_phone/snowstack
+//= require snowstack
 //= require jquery-ui.min
