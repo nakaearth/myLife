94d6fcce3139e5016f35f0978f630dffcd968e17
Nakamura shinichirou <naka5313@gmail.com>
2012-01-08 16:26:16 +0900

    aws setup add

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,5 @@
 #coding: utf-8
+require 'aws/s3'
 
 class Photo < ActiveRecord::Base
   belongs_to :album
@@ -8,11 +9,12 @@ class Photo < ActiveRecord::Base
   scope :latest  ,order('updated_at desc')
   validates :title  ,:presence=>true
   validates_attachment_size :photo ,:in =>1..5.megabyte,:message=>'ファイルサイズが大きすぎます' 
-  if :rails_env == 'production' 
+  if Rails.env.production? 
     has_attached_file :photo,
       :storage => :s3,
-      :path=>"photos/:id/:style/:filename",
-      :bucket=>'mybucket'
+      :s3_credentials=>"#{Rails.root}/config/s3.yml",
+      :url => ":s3_domain_url",
+      :path=>"photos/:id/:style/:filename"
   else 
     has_attached_file :photo,
       :url=>"/system/img/attaches/:id/:style/:filename" ,
--- config/initializers/aws.rb
+++
@@ -1,4 +0,0 @@
-AWS.config({
-  :access_key_id=> 'AKIAIHLLT4JODHTB3CEA',
-  :secret_access_key=> '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',
-})
