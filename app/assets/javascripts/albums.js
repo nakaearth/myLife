24030d1ade53624973d3fe20e51b9fc461ac29f3
Nakamura shinichirou <naka5313@gmail.com>
2012-02-25 13:09:25 +0900

    アバムブック機能追加

--- app/assets/javascripts/albums.js
+++ app/assets/javascripts/albums.js
@@ -3,5 +3,5 @@
 // be included in the compiled file accessible from http://example.com/assets/application.js
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
-//= require jquery1.7.7.min
+//= require jquery-1.7.1
 //= require turn
--- app/assets/javascripts/albums_smart_phone.js
+++ app/assets/javascripts/albums_smart_phone.js
@@ -4,4 +4,4 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //= require snowstack
-//= require jquery-ui.min
+//= require jquery-ui
