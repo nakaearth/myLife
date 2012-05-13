94765f81acc49007d71b33b20de195f8bfe8d591
Nakamura shinichirou <naka5313@gmail.com>
2012-05-13 11:05:13 +0900

    設定ファイルの内容を環境変数に変更。今更だけどね

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter,'HKSkVKAmsPNhAXEDmZP2w','9lu5yNeGjFWdfaA8nrizmHmJMty86a7vLNLbq8Vh4'
+  provider :twitter,ENV['TWITTER_ACCESS_TOKEN'],ENV['TWITTER_SECRET']
 end 
