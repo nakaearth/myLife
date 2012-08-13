1f5d6047209e1bae0257fd4f1072881435e269da
Nakamura shinichirou <naka5313@gmail.com>
2012-08-13 10:47:03 +0900

    omniauth update

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-#  provider :twitter,"7751092-0WGw7QM1RBLt7nbZtRPHkRBHt5efVrwlq9D31efNri","HLfQ7y2U50T7Tf7E7iYBGxF1Z8cAl4IUBcIFIQYNfTs"
-  provider :twitter,ENV["TWITTER_ACCESS_TOKEN"],ENV["TWITTER_SECRET"]
+  provider :twitter,"HKSkVKAmsPNhAXEDmZP2w","9lu5yNeGjFWdfaA8nrizmHmJMty86a7vLNLbq8Vh4"
+#  provider :twitter,ENV["TWITTER_ACCESS_TOKEN"],ENV["TWITTER_SECRET"]
 end 
