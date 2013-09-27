8e623086d17025e64fe5c32e63d1d3fb576ce3ed
Nakamura shinichirou <naka5313@gmail.com>
2013-09-27 23:51:09 +0900

    Delete omniauth.rb

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer unless Rails.env.production?
-  provider :twitter, "HKSkVKAmsPNhAXEDmZP2w", "9lu5yNeGjFWdfaA8nrizmHmJMty86a7vLNLbq8Vh4"
-#  provider :twitter, ENV["TWITTER_CONSUMER_KEY"], ENV["TWITTER_CONSUMER_SECRET"]
-end 
